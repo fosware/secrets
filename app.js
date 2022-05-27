b29ef7670a7d15e49f8545e695e9f9b3dd74945c
fosware <fosahadal@protonmail.com>
2022-05-27 16:21:49 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,55 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userBD");
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+      email:    req.body.email,
+      password: req.body.password
+    });
+
+    newUser.save(function(err){
+      if(err)
+        console.log(err);
+      else
+        res.render("secrets");
+    });
+});
+
+app.listen(3000, function(){
+  console.log("Server started at port 3000");
+});
